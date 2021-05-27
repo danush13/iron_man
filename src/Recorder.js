8d06897e510743e72a14351edad1f29419c164a2
praveen kumar <praveen.k@madstreetden.com>
2021-05-27 14:50:21 +0530

    Working code:

--- src/Recorder.js
+++ src/Recorder.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { io } from "socket.io-client";
+
+
+
+const ENDPOINT = "http://0.0.0.0:5000";
+
 
 class Recorder extends Component {
     constructor(props) {
@@ -7,7 +13,15 @@ class Recorder extends Component {
             recording: false,
             audioChunks: []
         }
+        this.chunks = []
+        this.socket = io(ENDPOINT);
+        this.socket.on("connect", () => {
+            console.log(this.socket.id);
+        });
+
     }
+
+    componentDidMount() { }
 
 
     recordAudio = async () => {
@@ -15,24 +29,65 @@ class Recorder extends Component {
         this.mediaRecorder = new MediaRecorder(this.stream)
         this.mediaRecorder.start(10)
         this.mediaRecorder.ondataavailable = e => {
-            if (e.data && e.data.size > 0) {
-                console.log('data has entered')
-                console.log(e.data)
-
-            }
+            this.chunks.push(e.data);
         };
+        // setInterval(() => {
+        //     console.log('after 3 seconds')
+        //     console.log(this.chunks[0].type)
+        //     const blob = new Blob(this.chunks, { 'type': this.chunks[0].type });
+        //     this.socket.emit('message', {audio: blob, type:this.chunks[0].type})
+        // }, 5000);
     }
     stopRecordingAudio = async () => {
-        console.log("stop has been called")
-        console.log(this.mediaRecorder)
+        // console.log("stop has been called")
+        // console.log(this.mediaRecorder)
         this.mediaRecorder.stop()
+        const blob = new Blob(this.chunks, { 'type': this.chunks[0].type });
+        this.socket.emit('message', { audio: blob, type: this.chunks[0].type })
+        this.socket.on("rec_message", data => {
+            console.log(data)
+            const blob = new Blob([data.audio], { type: data.type });
+            const audioUrl = URL.createObjectURL(blob);
+            const audio = new Audio(audioUrl);
+            audio.play();
+
+        });
+
+
+
     }
 
+    test = () => {
+        this.socket.emit("message", { name: "John" });
+    }
+
+    sendMessage = () => {
+        // this.socket.emit("message", bota(JSON.stringify({ name: "John" })));
+        this.ws.send("hello world")
+
+    }
+
+    // recordAudioV2 = async () => {
+    //     this.stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    //     this.recordAudio = RecordRTC(stream, {
+    //         type: 'audio',
+    //         mimeType: 'audio/webm',
+    //         sampleRate: 44100,
+    //         desiredSampRate: 16000,
+    //         recorderType: StereoAudioRecorder,
+    //         numberOfAudioChannels: 1
+    //     });
+    //     this.recordAudio.startRecording();
+    // }
+
     render() {
-        return(
+        return (
             <>
-            <button onClick={this.recordAudio}>Record</button>
-            <button onClick={this.stopRecordingAudio}>Stop</button>
+                <button onClick={this.recordAudio}>Record</button>
+                <button onClick={this.stopRecordingAudio}>Stop</button>
+                <button onClick={this.test}>test</button>
+                <button onClick={this.sendMessage}>send message</button>
+
             </>
         )
     }
